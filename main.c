3c44fd3a1c1dbfc9d26dac1f9481e58708d8a338
jim <61980746+jimgreekgamerYT@users.noreply.github.com>
2022-11-16 16:13:44 +0200

    Main code upload
    Added Lab1.cbp ( Run in code blocks ).
    Added the main code ( main.c ).
    Added extra needed file to run it.

---
+++ main.c
@@ -0,0 +1,36 @@
+/*  File: Lab1
+ *  Author: Dimitris Katsantonis
+ *
+ *  Created on October 12, 2022
+ *
+ *  A program that prints the address of variables and sizes ( in bytes ) of variable types
+ */
+
+#include <stdio.h>
+
+int main() {
+
+  unsigned int intVar1, intVar2;
+
+  printf("Print addresses of variables and values without initialization\n");
+  printf("Address of variables 'intVar1' is :%p and the value is: %u\n", &intVar1, intVar1 );
+  printf("Address of variables 'intVar2' is :%p and the value is: %u\n", &intVar2, intVar2 );
+
+  intVar1 = 1;
+  intVar2 = 2;
+
+  printf("\nPrint the addresses of the variables and their values after initialization\n");
+  printf("Address of the variable 'intVar1' is:%p and value is: %u\n", &intVar1, intVar1 );
+  printf("Address of the variable 'intVar2' is:%p and value is: %u\n", &intVar2, intVar2);
+
+  printf("\nA char is %u bytes", sizeof(char));
+  printf("\nA int is %u bytes", sizeof(int));
+  printf("\nA short is %u bytes", sizeof(short));
+  printf("\nA long is %u bytes", sizeof(long));
+  printf("\nA long long is %u bytes", sizeof(long long));
+  printf("\nA float is %u bytes", sizeof(float));
+  printf("\nA double is %u bytes", sizeof(double));
+
+  printf("\n");
+  return 0;
+  }
